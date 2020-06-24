d32e55dd554cb84fcdbac419ff66ec4f90f7495a
DwarfPorter <mrvlmor@gmail.com>
2020-06-24 21:41:12 +0300

    добоваил код поиск кнопки

--- app/src/main/java/ru/geekbrains/helloteam/MainActivity.java
+++ app/src/main/java/ru/geekbrains/helloteam/MainActivity.java
@@ -3,6 +3,7 @@ package ru.geekbrains.helloteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+
     }
 }
